6aae717e9595cc1db9d5cb6b5fb10ce97ca617c5
Nastasha <nastasha.alli@gmail.com>
2014-06-24 22:18:35 -0400

    javascript intro

---
+++ Nastasha/treehouse-javascript/myscript.js
@@ -0,0 +1,35 @@
+// Do a couple of console.logs
+
+console.log("Hello from myscript.js");
+console.log("Hello again!"); // This is not needed
+
+/*
+var name = prompt("What is your name?");
+alert("Hello " + name); 
+
+
+name = "Nick";
+console.log("The user's name is " + name);
+*/
+
+/*
+console.log("Before");
+
+var name = prompt("What is your name?");
+
+if (name) {
+	console.log("If block");
+} else {
+	console.log("Else block");
+}
+
+
+console.log("After"); */
+
+console.log("Before");
+
+while (true) {
+	console.log('Hello World!');
+}
+
+console.log("After");
